e8b6cf21350b3ab8f405dac04df2de647c4c1086
Lulo02 <luloalbarracin@gmail.com>
2021-02-20 16:26:14 -0500

    PRUEBA

---
+++ Puntos_Taller1/src/puntos_taller1/Puntos_Taller1.java
@@ -0,0 +1,19 @@
+
+package puntos_taller1;
+
+/**
+ *
+ * @author Lulo
+ */
+public class Puntos_Taller1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        System.out.println("LUISAA");
+        
+    }
+    
+}
